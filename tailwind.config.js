21b47485fe5bad2a7f5e201cfdf46d942fbf4eff
Md Akther Hosen <dev.aktherhosen@gmail.com>
2024-03-11 11:28:49 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {},
+    // screens: {
+    //   'sm': '440px'
+    // }
+  },
+  plugins: [],
+};
